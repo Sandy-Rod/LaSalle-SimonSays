d268a7b7a53ca53ab68ad172dabd96152ab8beca
Sandy Rodriguez <sandy.rodriguezaponte@hotmail.com>
2024-04-27 10:50:24 +0200

    primera parte del juego terminada

--- js/simon.js
+++ js/simon.js
@@ -1,12 +1,22 @@
 let buttons = []
+let game = [1]
+
+/** Generar número entero random entre 0 - 3*/
+function getRandomInt() {
+    return Math.floor(Math.random() * 4) + 1
+}
+
+
+/** Set up next turn */
+function setUpNextTurn() {
+    game.push(getRandomInt())
+    document.querySelector(".debug #current-game").innerText = game.join(" , ")
+}
 
 
 /** Juego inicial **/
-
 function initGame() {
-
     let button
-
     const main = document.querySelector("main")
 
     for (let i of [1, 2, 3, 4]) {
@@ -14,8 +24,13 @@ function initGame() {
         button.innerText = buttons.length + 1
         main.appendChild(button)
         buttons.push(button)
+        document.querySelector("#next-turn").addEventListener("click", setUpNextTurn)
     }
 }
+
+
+
+
 
 
 initGame()
